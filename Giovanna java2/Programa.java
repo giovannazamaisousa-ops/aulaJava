9889ef4299bb33ec6af663f94bd5f401074fc7f2
giovannazs <giovanna.zamai.sousa@escola.pr.gov.br>
2026-05-22 11:09:26 -0300

    Add files via upload

---
+++ Giovanna java2/Programa.java
@@ -0,0 +1,11 @@
+public class Programa {
+    public static void main(String[] args) {
+        String nome = "Giovanna";
+        int idade = 16;
+        double altura = 1.52;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+    }
+}
---
+++ Giovanna java2/programa.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Executando o programa java....
+java -cp "C:\Users\Aluno\Desktop\Giovanna java2" Programa
+pause 
